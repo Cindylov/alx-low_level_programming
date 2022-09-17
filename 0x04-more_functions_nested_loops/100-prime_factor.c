b2f0f633351da06f311083777895d6a998a6f479
Cindylov <cynthiaujunwa4@gmail.com>
2022-09-16 17:22:00 -0700

    done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+	unsigned long num = 612852475143;
+	unsigned long bd = num;
+	unsigned long bu = 1;
+	int divisor = 2;
+
+	while (1)
+	{
+		if ((bd % divisor) == 0)
+		{
+			bd = bd / divisor;
+			bu = bu * divisor;
+		}
+		else
+			divisor++;
+		if (bu >= num)
+			break;
+	}
+	printf("%d\n", divisor);
+}
