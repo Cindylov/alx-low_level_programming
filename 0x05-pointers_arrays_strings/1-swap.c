36de6c2d09995c3e483b731ebd75be8783079fce
Cindylov <cynthiaujunwa4@gmail.com>
2022-09-20 15:20:13 -0700

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,8 +5,8 @@
  * swap_int - switches value of first input with the value of the second input.
  * @a: First value to swap.
  * @b: Second value to swap.
- * Return: 0 (Success) 
- */
+ * Return: 0 (Success)
+ **/
 void swap_int(int *a, int *b)
 {
 	int temp;
