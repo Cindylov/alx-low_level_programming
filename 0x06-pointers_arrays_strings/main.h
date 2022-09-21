95e1157237b14a02d2a7e3c83d2801aff60c1589
Cindylov <cynthiaujunwa4@gmail.com>
2022-09-21 06:19:08 -0700

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 void positive_or_negative(int i);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+ar *_strncpy(char *dest, char *src, int n);
 
 
 #endif /* MAIN_H */
