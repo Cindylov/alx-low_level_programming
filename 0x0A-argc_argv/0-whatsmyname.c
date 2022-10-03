575931c0aa06fe5010625d421876aceaed51ad8a
Cindylov <cynthiaujunwa4@gmail.com>
2022-10-03 04:05:50 -0700

    ready

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a name
+ * @argc: arrgument count
+ * @argv: arrgument vector
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
