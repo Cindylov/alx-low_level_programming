fb039933cde7449957b50ccb9fd6981e26be0d47
Cindylov <cynthiaujunwa4@gmail.com>
2022-10-10 11:18:11 -0700

    check

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * check struct dog my_dog
+ * intializing a struct
+ * Return 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int main (void)
+{
+	struct dog my_dog;
+	my_dog.name = "poppy"
+		my_dog.age = 3.5
+		my_dog.owner = "Bob"
+		return (0);
+}
